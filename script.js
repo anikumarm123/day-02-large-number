caf1d2660263d12f4aca373f998bd5f3ce205a93
muthukumar-m <anikumar2825@gmail.com>
2022-06-02 12:29:22 +0530

    add-file.

---
+++ script.js
@@ -0,0 +1,11 @@
+//large number//
+let a = 10,b = 10;
+ if(a>b){
+    console.log("a is big number")
+ }
+ else if(a == b){
+     console.log("a and b are same number")
+ }
+else{   
+    console.log("b is big nummber")
+ }
